c78ab7d4f41bd0c19b8364be04ef61152b4ded1b
frostbitecloudhost <frostbitecloudhosting@gmail.com>
2024-02-02 12:43:18 +0530

    changes

--- signin.js
+++ signin.js
@@ -1,8 +1,6 @@
-// Import Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCYisJ9j4FqUAxdZRLbXbV0lARN9LhM0Mw",
   authDomain: "frostbite-cloud.firebaseapp.com",
@@ -14,33 +12,24 @@ const firebaseConfig = {
   measurementId: "G-49M1EZ9D2F"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// Function to update the sign-in text based on user authentication
 function updateSignInText() {
     var signInTextElement = document.getElementById("signInText");
 
-    // Get the current user from Firebase Authentication
     var user = auth.currentUser;
 
     if (user) {
-        // User is signed in
         signInTextElement.textContent = "Profile";
     } else {
-        // User is signed out
         signInTextElement.textContent = "Sign In";
     }
 }
 
-// Add an event listener to wait for the DOM to fully load
 document.addEventListener("DOMContentLoaded", function () {
     console.log("DOM Loaded");
-
-    // Add an observer to handle authentication state changes
     onAuthStateChanged(auth, function (user) {
-        // Update UI on authentication state changes
         updateSignInText();
     });
 });
